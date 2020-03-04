91be65fa3ec71b62a33221b8f3afdcc4c1e5b46c
akshayakky <akshaysmpt01@gmail.com>
2020-03-04 18:12:59 +0530

    ADDED : First Name Validation And Test Cases

--- src/main/java/ValidateUser.java
+++ src/main/java/ValidateUser.java
@@ -1,6 +1,12 @@
+import java.util.regex.Pattern;
 
 public class ValidateUser {
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Using JUnit");
+    }
+
+    public static boolean isValid(String firstName)
+    {
+        return Pattern.matches("^[A-Z][a-zA-Z]{2,}$",firstName);
     }
 }
--- src/test/java/UserValidationTestCases.java
+++ src/test/java/UserValidationTestCases.java
@@ -1,4 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UserValidationTestCases {
+    ValidateUser user = new ValidateUser();
 
+    @Test
+    public void givenFirstName_WhenValidFirstLetterUpperCase_ThenReturn() {
+        boolean result = user.isValid("Akshay");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenFirstName_WhenInvalidFirstLetterUpperCase_ThenReturn() {
+        boolean result = user.isValid("akshay");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenFirstName_WhenValidLength_ThenReturn() {
+        boolean result = user.isValid("Aks");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenFirstName_WhenInvalidLength_ThenReturn() {
+        boolean result = user.isValid("Ak");
+        Assert.assertFalse(result);
+    }
 }
