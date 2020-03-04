298aa09e32540701dc1454cb8fc933f5a80e41b6
akshayakky <akshaysmpt01@gmail.com>
2020-03-04 18:34:45 +0530

    ADDED : Last Name Validation And Test Cases

--- src/main/java/ValidateUser.java
+++ src/main/java/ValidateUser.java
@@ -5,7 +5,12 @@ public class ValidateUser {
         System.out.println("Welcome To User Registration Using JUnit");
     }
 
-    public static boolean isValid(String firstName)
+    public static boolean isValidFirstName(String firstName)
+    {
+        return Pattern.matches("^[A-Z][a-zA-Z]{2,}$",firstName);
+    }
+
+    public static boolean isValidLastName(String firstName)
     {
         return Pattern.matches("^[A-Z][a-zA-Z]{2,}$",firstName);
     }
--- src/test/java/UserValidationTestCases.java
+++ src/test/java/UserValidationTestCases.java
@@ -6,25 +6,49 @@ public class UserValidationTestCases {
 
     @Test
     public void givenFirstName_WhenValidFirstLetterUpperCase_ThenReturn() {
-        boolean result = user.isValid("Akshay");
+        boolean result = user.isValidFirstName("Akshay");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenFirstName_WhenInvalidFirstLetterUpperCase_ThenReturn() {
-        boolean result = user.isValid("akshay");
+        boolean result = user.isValidFirstName("akshay");
         Assert.assertFalse(result);
     }
 
     @Test
     public void givenFirstName_WhenValidLength_ThenReturn() {
-        boolean result = user.isValid("Aks");
+        boolean result = user.isValidFirstName("Aks");
         Assert.assertTrue(result);
     }
 
     @Test
     public void givenFirstName_WhenInvalidLength_ThenReturn() {
-        boolean result = user.isValid("Ak");
+        boolean result = user.isValidFirstName("Ak");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_WhenValidFirstLetterUpperCase_ThenReturn() {
+        boolean result = user.isValidFirstName("Kharade");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastName_WhenInvalidFirstLetterUpperCase_ThenReturn() {
+        boolean result = user.isValidFirstName("kharade");
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void givenLastName_WhenValidLength_ThenReturn() {
+        boolean result = user.isValidFirstName("Kha");
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void givenLastName_WhenInvalidLength_ThenReturn() {
+        boolean result = user.isValidFirstName("Kh");
         Assert.assertFalse(result);
     }
 }
